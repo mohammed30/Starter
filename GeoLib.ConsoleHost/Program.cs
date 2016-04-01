9ae7e4a1109b16ba39897fa0a84c24fb1d65cc74
Kareem <Kareem@DESKTOP-V91KEDE>
2016-04-01 23:33:05 +0200

    Add project files.

---
+++ GeoLib.ConsoleHost/Program.cs
@@ -0,0 +1,26 @@
+﻿using GeoLib.Contracts;
+using GeoLib.Services;
+using System;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+
+namespace GeoLib.ConsoleHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ServiceHost hostGeoManager = new ServiceHost(typeof(GeoManager));
+            string address = "net.tcp://localhost:4497/GeoService";
+            Binding binding = new NetTcpBinding();
+            Type contract = typeof(IGeoService);
+            hostGeoManager.AddServiceEndpoint(contract, binding, address);
+            hostGeoManager.Open();
+            Console.WriteLine("Services started. Press [Enter] to exit.");
+            Console.ReadLine();
+            hostGeoManager.Close();
+
+        }
+    }
+}
---
+++ GeoLib.WebHost/CustomHostFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.Web;
+
+namespace GeoLib.WebHost
+{
+    public class CustomHostFactory : ServiceHostFactory
+    {
+        protected override ServiceHost CreateServiceHost(Type serviceType, Uri[] baseAddresses)
+        {
+            ServiceHost Host = new ServiceHost(serviceType, baseAddresses);
+            return Host;
+        }
+    }
+}
